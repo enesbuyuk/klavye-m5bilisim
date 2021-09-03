bea6597029de485820b3e9703ef113fa6b9892d3
enesbuyuk <82279640+enesbuyuk@users.noreply.github.com>
2021-09-03 05:23:45 +0300

    Update hiz-testi.js

--- hiz-testi.js
+++ hiz-testi.js
@@ -2,11 +2,10 @@ function enes(){
     const dizi = document.getElementById('satir').textContent.split(" ");
     var strokeCount = -1;
 	document.body.onkeyup = function(e){
-        if(e.keyCode == 32){
-            let enes = ++strokeCount;
-            document.getElementById('yaziyaz').value = dizi[enes]; 
-    
-        }
+		if(e.keyCode == 32){
+		    let enes = ++strokeCount;
+		    document.getElementById('yaziyaz').value = dizi[enes]; 
+		}
 	}
 }
 enes();
