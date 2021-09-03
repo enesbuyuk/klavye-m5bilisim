37fe8aaa411b9b611b50582f0da370b163828eb1
enesbuyuk <82279640+enesbuyuk@users.noreply.github.com>
2021-09-03 05:24:26 +0300

    Update duello.js

--- duello.js
+++ duello.js
@@ -1,9 +1,9 @@
 function enes(){
     const dizi = document.getElementById('yazilacak-metin').textContent.split(" ");
-    var strokeCount = -1;
+    var sayi = -1;
 	document.body.onkeyup = function(e){
 		if(e.keyCode == 32){
-		    let enes = ++strokeCount;
+		    let enes = ++sayi;
 		    document.getElementById('yazilan-metin').value = dizi[enes]; 
 		}
 	}
