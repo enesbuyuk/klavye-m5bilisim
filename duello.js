2655700ccefeacf62b7a5b44262274345b5bca0f
enesbuyuk <82279640+enesbuyuk@users.noreply.github.com>
2021-09-03 05:24:07 +0300

    Update duello.js

--- duello.js
+++ duello.js
@@ -2,11 +2,10 @@ function enes(){
     const dizi = document.getElementById('yazilacak-metin').textContent.split(" ");
     var strokeCount = -1;
 	document.body.onkeyup = function(e){
-        if(e.keyCode == 32){
-            let enes = ++strokeCount;
-            document.getElementById('yazilan-metin').value = dizi[enes]; 
-    
-        }
+		if(e.keyCode == 32){
+		    let enes = ++strokeCount;
+		    document.getElementById('yazilan-metin').value = dizi[enes]; 
+		}
 	}
 }
 enes();
