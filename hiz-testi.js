f2d2386426bdc09a7f0b56b31ce32a64790fafdc
Enes Büyük <contact@enesbuyuk.com>
2022-12-26 15:59:34 +0300

    Update hiz-testi.js

--- hiz-testi.js
+++ hiz-testi.js
@@ -1,11 +1,11 @@
-function enes(){
+function hizTesti(){
     const dizi = document.getElementById('satir').textContent.split(" ");
     var sayi = -1;
 	document.body.onkeyup = function(e){
 		if(e.keyCode == 32){
-		    let enes = ++sayi;
-		    document.getElementById('yaziyaz').value = dizi[enes]; 
+		    let index = ++sayi;
+		    document.getElementById('yaziyaz').value = dizi[index]; 
 		}
 	}
 }
-enes();
+hizTesti();
