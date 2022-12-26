959e83fff77e0b99862dae284973a7d7b7d94f38
Enes Büyük <contact@enesbuyuk.com>
2022-12-26 15:59:53 +0300

    Update duello.js

--- duello.js
+++ duello.js
@@ -1,11 +1,11 @@
-function enes(){
+function duello(){
     const dizi = document.getElementById('yazilacak-metin').textContent.split(" ");
     var sayi = -1;
 	document.body.onkeyup = function(e){
 		if(e.keyCode == 32){
-		    let enes = ++sayi;
-		    document.getElementById('yazilan-metin').value = dizi[enes]; 
+		    let index = ++sayi;
+		    document.getElementById('yazilan-metin').value = dizi[index]; 
 		}
 	}
 }
-enes();
+duello();
